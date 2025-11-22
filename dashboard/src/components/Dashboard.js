f0dc59c4f548c7c9cbfce90a8f37eef42447194e
Saloni Tomar <tomarsaloni19@gmail.com>
2025-11-22 19:54:31 +0530

    Adding Pricing Component in Frontend

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import Apps from "./Apps";
 import Funds from "./Funds";
 import Holdings from "./Holdings";
 
-import Orders from "./Orders";
+// import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
@@ -19,7 +19,7 @@ const Dashboard = () => {
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
-          <Route path="/orders" element={<Orders />} />
+          {/* <Route path="/orders" element={<Orders />} /> */}
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
           <Route path="/funds" element={<Funds />} />
